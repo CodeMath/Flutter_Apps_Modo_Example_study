729232809213cb08657b3db0874d73759421010d
CodeMath <xyrho123@naver.com>
2022-11-11 21:49:02 +0900

    이미지 로컬 저장

---
+++ lib/services/modo_file_service.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
+
+Future<String> saveImageToLocalDirectory(File image) async {
+  final documentDirectory = await getApplicationDocumentsDirectory();
+
+  /// to interpolation
+  final folderPath = '${documentDirectory.path}/medicine/images';
+  final filepath = '$folderPath/${DateTime.now().millisecondsSinceEpoch}.png';
+
+  await Directory(folderPath).create(recursive: true);
+
+  final newFile = File(filepath);
+  newFile.writeAsBytesSync(image.readAsBytesSync());
+
+  return filepath;
+}
